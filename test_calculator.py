9d949f05ebe01161af807a3def007c3c41196797
Yves Simenya <>
2019-02-23 22:28:51 +0100

    Add tests for addition and substraction

---
+++ test_calculator.py
@@ -0,0 +1,42 @@
+"""
+Unit tests for the calculator library
+"""
+
+
+import calculator
+
+
+class TestCalculator:
+    def test_addition(self):
+        assert 4 == calculator.add(1, 3)
+
+    def test_subtraction(self):
+        assert 5 == calculator.sub(8, 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
